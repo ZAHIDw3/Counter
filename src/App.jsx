f1427fa2c76499cb83250bbbc3564094fd07efea
Zahid de Jesus Avalos Hernandez <l19350283@tuctepec.tecnm.mx>
2023-03-02 09:07:05 -0600

    restar 1 al contador y reiniciarlo

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,21 @@ function App() {
       <h3>Counter</h3>
       <hr />
       <h1>{counter}</h1>
-      <button onClick={() => {setCounter(counter +1); console.log(counter)}} type="button" className="btn btn-primary">+1</button>
+      <button 
+        onClick={() => {setCounter(counter +1); console.log(counter)}} 
+        type="button" 
+        className="btn btn-primary"
+      >+1</button>
+      <button 
+        onClick={() => {setCounter(counter -1); console.log(counter)}} 
+        type="button" 
+        className="btn btn-warning"
+      >-1</button>
+      <button 
+        onClick={() => {setCounter(0); console.log(counter)}} 
+        type="button" 
+        className="btn btn-danger"
+      >Reset counter</button>
     </div>
   )
 }
